7aae46a616ba55b16a036a402968b9260593d004
zhangda5 <57501170+zhangda5@users.noreply.github.com>
2025-10-22 21:29:33 -0400

    Update bronze-volume-setup.sql

--- src/Table creation/bronze-volume-setup.sql
+++ src/Table creation/bronze-volume-setup.sql
@@ -12,7 +12,7 @@
 
 -- DBTITLE 1,Parameters
 -- MAGIC %python
--- MAGIC dbutils.widgets.text('storageaccountname', 'demolab')
+-- MAGIC dbutils.widgets.text('storageaccountname', 'demorzfabric')
 
 -- COMMAND ----------
 
